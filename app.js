adffac7bd51d18cc5ab2416e29254f9e09c4a1e1
TC <nothing@nothing.com>
2025-02-22 15:43:14 +1300

    Add some styling and messages to index.ejs

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 const indexRouter = require("./routes/index");
 const newRouter = require("./routes/new");
@@ -8,10 +13,6 @@ const PORT = 5051;
 
 app.use("/", indexRouter);
 app.use("/new", newRouter);
-
-// app.get("/", (req, res) => {
-//   res.send("hello");
-// });
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${5051}`);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
+// const path = require("path");
+
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view enginer", "ejs");
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 router.get("/", (req, res) => {
-  res.send("Hello again");
+  res.render("index", { messages: messages });
 });
 
 module.exports = router;
