16d50fcf5d25031f9ac35e143b80c164376ebe77
TC <nothing@nothing.com>
2025-02-22 16:40:29 +1300

    Add message.ejs to show seperate messages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
 
+router.get("/message/:id", (req, res) => {
+  const message = messages[req.params.id];
+  if (!message) {
+    return res.status(404).send("Message not found");
+  }
+  res.render("message", { message: message });
+});
+
 module.exports = router;
